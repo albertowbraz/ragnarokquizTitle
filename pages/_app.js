e0344625f5b845a98bcd2a2b7ebedabb192fbb46
albertowbraz <albertowbraz>
2021-01-25 19:28:52 +0000

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json';
+import Head from 'next/head'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+const theme = db.theme;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+      <title>Ragnarok Quiz Alura</title>
+        <meta name="test" content="initial-scale=1.0, width=device-width" />
+        
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
